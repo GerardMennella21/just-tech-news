41b3a0df88f9241a237fbe9c054a49fcd83de6ff
Gerard Mennella <gerardmennella21@gmail.com>
2022-05-25 03:37:48 -0400

    closes #4

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -17,7 +17,19 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     }).then(dbUserData => {
         if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id' });
@@ -53,6 +65,7 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'No user with that email address!' })
             return
         }
+        
         const validPassword = dbUserData.checkPassword(req.body.password);
 
         if (!validPassword) {
